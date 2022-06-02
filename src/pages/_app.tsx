95bb7fef62e096e986e1a4dd0629aaa7dca4bd2a
oberdansoldi <oberdan_soldi@outlook.com.br>
2022-06-02 20:54:40 -0300

    init: iniciando projeto e realizando configurações base

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../ui/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ThemeProvider} from "@mui/material";
+import theme from "../ui/themes/themes";
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
+}
+
+export default MyApp
